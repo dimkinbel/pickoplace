1522d0dd15ccda1d0004b6038b941625fe654ba4
Egor Davydov <evdavydov@gmail.com>
2015-10-29 23:58:54 +0300

    issue-2 - work started

---
+++ src/main/java/com/dimab/pickoplace/i18n/I18nFilter.java
@@ -0,0 +1,21 @@
+package com.dimab.pickoplace.i18n;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class I18nFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // no-op
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        // no-op
+    }
+}
---
+++ src/main/webapp/js/services/i18n.js
@@ -0,0 +1,18 @@
+!function() {
+
+    var LOCALES = {
+        english: {
+            imageUrl: '' // todo(egor): add flags
+        },
+        hebrew: {
+            imageUrl: ''
+        }
+    };
+
+    // todo(egor): implement
+    var i18n = {
+        get: function(messageKey) {
+
+        }
+    };
+}();
