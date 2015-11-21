8260f80a5441affdd97161467846e834a7a4a5fb
Egor Davydov <evdavydov@gmail.com>
2015-11-21 22:17:35 +0300

    add MVC (in progress)

--- src/main/java/com/dimab/pickoplace/PickoplaceApplication.java
+++ src/main/java/com/dimab/pickoplace/PickoplaceApplication.java
@@ -1,19 +1,21 @@
 package com.dimab.pickoplace;
 
-import com.dimab.pickoplace.controller.TestController;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletProperties;
 
 public class PickoplaceApplication extends ResourceConfig {
 
     public PickoplaceApplication() {
-//        packages("com.dimab.pickoplace.controller");
-        register(TestController.class);
+        packages("com.dimab.pickoplace.controller");
+        packages("com.dimab.pickoplace.rest");
 
         property(ServletProperties.FILTER_FORWARD_ON_404, true);
 
         // register(LoggingFilter.class); // for dev purposes
 
         register(org.glassfish.jersey.server.mvc.MvcFeature.class);
+
+        register(JacksonFeature.class);
     }
 }
---
+++ src/main/java/com/dimab/pickoplace/jersey/ObjectMapperProvider.java
@@ -0,0 +1,29 @@
+package com.dimab.pickoplace.jersey;
+
+// todo(egor): implement
+public class ObjectMapperProvider {
+/*
+        implements ContextResolver<ObjectMapper> {
+
+    final ObjectMapper defaultObjectMapper;
+
+    public MyObjectMapperProvider() {
+        defaultObjectMapper = createDefaultMapper();
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return defaultObjectMapper;
+    }
+}
+
+    private static ObjectMapper createDefaultMapper() {
+        final ObjectMapper result = new ObjectMapper();
+//        result.configure(Feature.INDENT_OUTPUT, true);
+
+        return result;
+    }
+
+    // ...
+*/
+}
